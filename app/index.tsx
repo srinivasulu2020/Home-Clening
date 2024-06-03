a6875eebf1db8c3d8695edd586220a7bca0c3b01
srinivas <srinuwork2020@gmail.com>
2024-06-03 22:45:55 +0530

    page create in new

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function homeservice() {
+  return (
+    <View>
+      <Text>HOME SERVICE</Text>
+    </View>
+  )
+}
